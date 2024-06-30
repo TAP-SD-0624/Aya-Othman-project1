35b7ff9893cf597d981dbd6585309b542f1fb884
Aya Othman <khaled.aya444@gmail.com>
2024-06-30 14:24:33 +0300

    hehe

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
+const body = document.querySelector('body');
+const btnDark = document.querySelector('.btnDark');
+const modeText = document.querySelector('.modeText');
+const btnFavourite=document.querySelector('.btnFavourite');
+const favouriteContainer=document.querySelector('.favouriteContainer');
+
+btnDark.addEventListener("click", () => {
+    
+body.classList.contains("dark")
+? body.classList.toggle("lightMode")
+: body.classList.toggle("darkMode");
+});
+
+btnDark.addEventListener("click", () => {
+    if (modeText.innerText === "Dark Mode") {
+        modeText.innerText = "Light Mode";
+    } else {
+        modeText.innerText = "Dark Mode";
+    }
+});
+
+btnFavourite.addEventListener("click", () => {
+  favouriteContainer.classList.toggle("showFavourite");
+  console.log("show");
+});
+
+
 let topics=[];
-let faivouriteList=[];
+let favouriteList=[];
 document.addEventListener("DOMContentLoaded", () => {
     fetch("topics.json")
       .then((response) => response.json())
       .then((data) => {
-        topics=data;
+        topics=data;       
+        favouriteList = topics.filter((item) => item.favourite);
         console.log (data);
-/* call func favourate here */
+        createFavouritesTopicsCards(favouriteList);
+        console.log(favouriteList);
         createTopicsCards(data);
+        
       });
   });
 
@@ -46,34 +76,35 @@ document.addEventListener("DOMContentLoaded", () => {
       });
      }); 
   };
+
   
-const faivourite= (fav)=>{
-    const innerContainer = document.querySelector(".innerContainer");
+  
+const createFavouritesTopicsCards= (fav)=>{
+  const favContainer = document.querySelector(".favouriteList");
+  fav.map((favCourse) => {
+    const cardCourse= document.createElement("div");
+    cardCourse.classList.add("card");
+    let cardHTML='';
+  cardHTML += `
+      <div class="favCourseImage">
+        <img src="${favCourse.image}" alt="${favCourse.topic}">
+      </div>
+      <div class="favCourseInfo">
+        <h3>${favCourse.topic}</h3>
+        <ul class="favUl">
+          <li><ion-icon class="starShap" name="star"></ion-icon></li>
+          <li><ion-icon  class="starShap" name="star"></ion-icon></li>
+          <li><ion-icon  class="starShap" name="star"></ion-icon></li>
+          <li><ion-icon  class="starShap" name="star"></ion-icon></li>
+          <li><ion-icon class="starShap"  name="star"></ion-icon></li>
+        </ul>
+    </div>
+  `;
+  cardCourse.innerHTML=cardHTML;
+  favContainer.appendChild(cardCourse);
 
-
+     }); 
 }
 /*1. add boolean to each element in jison property e.g. fav= fales as initial value 
 /*2. favourites = [...courses.filter((item) => item.isFavourite)];
 3. do mapping */
-
-const body = document.querySelector('body');
-const btnDark = document.querySelector('.btnDark');
-const modeText = document.querySelector('.modeText');
-
-btnDark.addEventListener("click", () => {
-    if (body.classList.contains("darkMode")) {
-        body.classList.toggle("lightMode");
-        body.classList.remove("darkMode");
-    } else {
-        body.classList.toggle("darkMode");
-        body.classList.remove("lightMode");
-    }
-});
-
-btnDark.addEventListener("click", () => {
-    if (modeText.innerText === "Dark Mode") {
-        modeText.innerText = "Light Mode";
-    } else {
-        modeText.innerText = "Dark Mode";
-    }
-});
